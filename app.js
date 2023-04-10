65ec4236833f17b35b71e8f06c4ee1f42be5ac74
Justine Casiano <justinecasiano30@gmail.com>
2023-04-10 20:14:31 +0800

    fix bugs especially cors and runtime, add logging of uploads to google sheets, and improve performance

--- app.js
+++ app.js
@@ -1,67 +1,113 @@
-const { timestamp, verify, validateInput } = require('./lib/utilities');
+const { login, validateInput } = require('./lib/utilities');
 const { createCertificate } = require('./lib/create-certificate');
 
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const crypto = require('crypto');
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
-let count = 0;
-const clients = [];
+let clients = [];
+let started = false;
+
 const PORT = process.env.PORT || 3001;
+const corsOptions = { origin: ['https://icertify.vercel.app'] };
 
-app.listen(PORT, () => console.log(`${timestamp()}: (INFO) server has started in port ${PORT}`));
+app.listen(PORT, () => console.log(`(INFO) server has started in port ${PORT}`));
 
-app.use(cors({ origin: 'https://icertify.vercel.app', credentials: true }));
+app.use(cors(corsOptions));
+
+app.use(allow);
+
+app.get('/start', start);
 
 app.get('/logging/:id', sendMessage);
 
-app.post('/verify', [upload.none(), verify]);
+app.post('/login', [upload.none(), login]);
 
 app.post('/generate', [logger, upload.single('file'),
   validateInput, createCertificate]);
 
+// middlewares that need access to global variables
+function allow(req, res, next) {
+  const origin = req.get('origin')
+  if (corsOptions.origin.includes(origin)) {
+    return next();
+  }
+  res.status(404).end();
+}
+
+function start(req, res) {
+  if (!started) {
+    started = true;
+    console.log('(INFO) client has started the server');
+  }
+  res.end();
+}
+
+function getClient(id) {
+  return clients.find(client => client.id === id);
+}
+
+function removeClient(client) {
+  return function () {
+    let id = client?.id;
+    if (getClient(id)) {
+      console.log(`(INFO) a client has disconnected ${id}`);
+
+      clearInterval(client.intervalID);
+      clients =  clients.filter(client => client.id !== id);
+
+      console.log('(INFO) clients connected', clients);
+    }
+  }
+}
+
 function logger(req, res, next) {
+  console.log('(INFO) a client has connected');
+  const id = crypto.randomUUID();
+  
   clients.push({
-    id: count,
+    id: id,
+    intervalID: 0,
     messages: [],
     newMessage: function (message) {
       this.messages.push(message);
-    },
-    intervalID: 0
+    }
   });
 
-  res.client = clients[count++];
+  res.client = getClient(id);
   res.client.newMessage('server accepted request');
   res.send(`/logging/${res.client.id}`);
-
-  console.log(`${timestamp()}: (INFO) a client has connected`)
   next();
 };
 
 function sendMessage(req, res) {
+  res.messageCount = 0;
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
-  res.messageCount = 1;
 
-  let client = clients[req?.params?.id];
+  let client = getClient(req?.params?.id);
+
+  req.on('close', removeClient(client));
+
   client.intervalID = setInterval(() => {
-    const client = clients[req?.params?.id];
-    if (client?.messages?.at(0) === 'END') {
-      clearInterval(client?.intervalID); 
-      count--;
+    if (client.messages?.at(0) === 'END') {
+      removeClient(client)();
     }
-    if (client?.messages?.length) {
+
+    if (client.messages.length) {
       res.write(`retry: ${1500}\n`);
       res.write(`data: ${client.messages.shift()}\n\n`);
-      res.write(`id: ${res.messageCount++}\n`);
+      res.write(`id: ${++res.messageCount}\n`);
     }
   }, 300);
+  console.log(clients);
 }
 
 
